0e443a1db634618c310008ffa9b1ec306a1fe79c
Ben <bch46@cornell.edu>
2014-04-28 21:35:37 -0400

    Write cell phone signal to a file on the SD card

--- WhateverTheWeather/src/info4120/whatevertheweather/MainActivity.java
+++ WhateverTheWeather/src/info4120/whatevertheweather/MainActivity.java
@@ -1,5 +1,7 @@
 package info4120.whatevertheweather;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.List;
 
 import android.app.Activity;
@@ -7,16 +9,23 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private SensorManager mSensorManager;
     private TextView mSensorsTot, mSensorAvailables, mCellSignal;
+    private Button toggleSensors, recordInput;
+    private boolean sensorsShowing, currentlyRecording;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,26 @@ public class MainActivity extends Activity {
         mSensorsTot = (TextView) findViewById(R.id.sensor_total);
         mSensorAvailables = (TextView) findViewById(R.id.sensor_avail);
         mCellSignal = (TextView) findViewById(R.id.cell_signal);
+        
+        mSensorAvailables.setVisibility(View.GONE);
+        sensorsShowing = false;
+        
+        toggleSensors = (Button) findViewById(R.id.show_sensors);
+        toggleSensors.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                toggleSensorList();
+            }
+        });
+        
+        currentlyRecording = false;
+        recordInput = (Button) findViewById(R.id.start_recording);
+        recordInput.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                toggleRecording();
+            }
+        });
 
         // Get the SensorManager
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -34,7 +63,7 @@ public class MainActivity extends Activity {
         List<Sensor> msensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
 
         // Print how may Sensors are there
-        mSensorsTot.setText(msensorList.size() + " " + this.getString(R.string.sensors) + "!");
+        mSensorsTot.setText(msensorList.size() + " " + this.getString(R.string.sensors));
 
         // Print each Sensor available using sSensList as the String to be
         // printed
@@ -68,6 +97,7 @@ public class MainActivity extends Activity {
 
             @Override
             public void onSignalStrengthsChanged(final SignalStrength strength) {
+                final long timeStamp = System.currentTimeMillis();
                 MainActivity.this.runOnUiThread(new Thread() {
                     @Override
                     public void run() {
@@ -81,10 +111,13 @@ public class MainActivity extends Activity {
                             msg = msg + " (Ec/Io: " + strength.getCdmaEcio() + ")";
                             msg = msg + "\nEVDO RSSI: " + strength.getEvdoDbm() + "dBm";
                             msg = msg + " (Ec/Io: " + strength.getEvdoEcio();
-                            msg = msg + ", SN/R: " + strength.getEvdoSnr() + ")";
-                            msg = msg + "\n" + strength.toString();
-                            System.out.println(msg);
+                            msg = msg + ", SN/R: " + strength.getEvdoSnr() + ")\n";
                             updateCellSignalStrength(msg);
+                            if (currentlyRecording) {
+                                logMessage(msg, timeStamp);
+                                System.out.println(strength.toString());
+                                System.out.println(msg);
+                            }
                         }
                     }
                 });
@@ -106,6 +139,37 @@ public class MainActivity extends Activity {
 
     protected void updateCellSignalStrength(String s) {
         mCellSignal.setText(s);
+    }
+
+    private void toggleSensorList() {
+        if (sensorsShowing) {
+            mSensorAvailables.setVisibility(View.GONE);
+            toggleSensors.setText(R.string.show_sensors);
+        } else {
+            mSensorAvailables.setVisibility(View.VISIBLE);
+            toggleSensors.setText(R.string.hide_sensors);
+        }
+        sensorsShowing = !sensorsShowing;
+    }
+    
+    private synchronized void toggleRecording() {
+        recordInput.setText(currentlyRecording ? R.string.start_recording : R.string.stop_recording);
+        currentlyRecording = !currentlyRecording;
+    }
+    
+    private void logMessage(String msg, long timeStamp) {
+        File myFile =
+                new File(Environment.getExternalStorageDirectory().getPath(), "data.txt");
+        try {
+            // create a filewriter and set append modus to true
+            FileWriter fw = new FileWriter(myFile, true);
+            msg = "==========\nTimestamp: " + timeStamp + "\n" + msg;
+            fw.append(msg);
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
